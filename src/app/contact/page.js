8386f209d1bd29798d5e69d2d27bf5f44cbf357b
Dtemplarr <nine9164@gmail.com>
2024-11-07 19:06:44 +0000

    update

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -98,7 +98,7 @@ export default function Contact() {
               value={formData.message}
               onChange={handleChange}
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 dark:text-white dark:bg-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Your Message"
+              placeholder="Your Message."
             ></textarea>
           </div>
 
